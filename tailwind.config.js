02b640279ff6fbd94ffc9763dbb369e7aca08e98
valjean <valjean@foxmail.com>
2023-01-03 15:08:39 +0800

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./learn/src/html/*.html",
+        "./learn/src/js/*.js"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
