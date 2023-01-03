14cd33deae0d1165da23dff366a34ffaa76cacb5
valjean <valjean@foxmail.com>
2023-01-03 18:45:18 +0800

    * framework ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    mode: "jit",
     content: [
         "./learn/src/html/*.html",
         "./learn/src/js/*.js"
     ],
     theme: {
-        extend: {},
+        extend: {
+            height: {
+                "header-height": "var(--header-height)",
+                "footer-height": "var(--footer-height)",
+                // "content-height": "calc(100vh - var(--header-height) - var(--footer-height))",
+                "content-height": "calc(100vh - var(--header-height))",
+            },
+        }
     },
     plugins: [],
 }
