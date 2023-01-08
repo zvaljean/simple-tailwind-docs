b0e3bc9042a5d9791757f5ea6891be142ec0a161
valjean <valjean@foxmail.com>
2023-01-08 20:18:27 +0800

    * nav sidebar

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,32 +1,34 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-    mode: "jit",
-    content: [
-        "./themes/**/layouts/**/*.html",
-        "./content/**/layouts/**/*.html",
-        "./layouts/**/*.html",
-        "./content/**/*.html"
-    ],
-    theme: {
-        extend: {
-            height: {
-                "header-height": "var(--header-height)",
-                "footer-height": "var(--footer-height)",
-                "content-height": "calc(100vh - var(--header-height))",
-            },
-        },
-        listStyleType: {
-            none: 'none',
-            disc: 'disc',
-            decimal: 'decimal',
-            square: 'square',
-            roman: 'upper-roman',
-        }
+  mode: "jit",
+  content: [
+    "./themes/**/layouts/**/*.html",
+    "./content/**/layouts/**/*.html",
+    "./layouts/**/*.html",
+    "./content/**/*.html"
+  ],
+  theme: {
+    extend: {
+      height: {
+        "header-height": "var(--header-height)",
+        "footer-height": "var(--footer-height)",
+        "main-height": "calc(100vh - var(--header-height))",
+        "content-height": "calc(100vh - var(--header-height) - var(--footer-height ) )"
+      }
     },
-    plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/line-clamp'),
-    ]
-}
+    listStyleType: {
+      none: "none",
+      disc: "disc",
+      decimal: "decimal",
+      square: "square",
+      roman: "upper-roman"
+    }
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+    require("tailwind-scrollbar")
+  ]
+};
