1e6c2e55dfae802d3e6d9525c2bc13b7cd2bdded
valjean <valjean@foxmail.com>
2023-02-08 23:16:33 +0800

    fix index template

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -5,21 +5,25 @@ const themeDir = __dirname + "/../../";
 
 const purgecss = require("@fullhuman/postcss-purgecss")({
   // see https://gohugo.io/hugo-pipes/postprocess/#css-purging-with-postcss
-  content: ["./hugo_stats.json", themeDir + "/hugo_stats.json", "exampleSite/hugo_stats.json"],
+  content: [
+    "./hugo_stats.json",
+    themeDir + "/hugo_stats.json",
+    "exampleSite/hugo_stats.json"
+  ],
   safelist: [/type/],
   defaultExtractor: (content) => {
     let els = JSON.parse(content).htmlElements;
     return els.tags.concat(els.classes, els.ids);
-  },
+  }
 });
 
 module.exports = {
   plugins: [
     require("tailwindcss")(themeDir + "assets/css/tailwind.config.js"),
     require("autoprefixer")({
-      path: [themeDir,
+      path: [themeDir]
     }),
-    ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : [],
+    ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : [])
     // ...(process.env.HUGO_ENVIRONMENT === "development" ? [purgecss] : []),
-  ,
+  ]
 };
